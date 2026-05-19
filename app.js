9a75029f6ebca813c3a10df73ce0ee67fbbcfc1d
MADHU871 <mad0008271@gmail.com>
2026-05-19 14:46:35 +0530

    Added advanced DevOps dashboard

--- app.js
+++ app.js
@@ -1,7 +1,88 @@
-function showMessage(){
+console.log("DevOps Dashboard Running 🚀");
 
-    alert("DevOps Deployment Started Successfully 🚀");
+/* Chart.js Dashboard */
+
+const ctx = document.getElementById('devopsChart');
+
+new Chart(ctx, {
+
+type: 'bar',
+
+data: {
+
+labels: [
+
+'Docker',
+'Jenkins',
+'GitHub',
+'Kubernetes',
+'CI/CD'
+
+],
+
+datasets: [{
+
+label: 'System Performance %',
+
+data: [95,90,98,85,99],
+
+borderWidth:2
+
+}]
+},
+
+options: {
+
+responsive:true,
+
+plugins: {
+
+legend: {
+
+labels: {
+
+color:'white'
 
 }
+}
+},
 
-console.log("Cloud Build Website Running");
+scales: {
+
+x: {
+
+ticks: {
+
+color:'white'
+
+}
+},
+
+y: {
+
+ticks: {
+
+color:'white'
+
+},
+
+beginAtZero:true,
+
+max:100
+
+}
+}
+}
+});
+
+/* AI Chatbot */
+
+function chatBot(){
+
+let input = document.getElementById('userInput').value;
+
+document.getElementById('response').innerHTML =
+
+"AI Response: Jenkins Build Successful 🚀";
+
+}
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+
+res.sendFile(
+path.join(__dirname,'public/index.html')
+);
+
 });
 
 app.listen(3000,()=>{
-    console.log("Server Running on Port 3000");
+
+console.log("DevOps Dashboard Running on Port 3000");
+
 });
